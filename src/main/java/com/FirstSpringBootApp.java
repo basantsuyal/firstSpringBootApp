22e22b2bd91b1d7bf4ce7ba5381db9a679ab6b78
Basant Suyal <basant.suyal@gmail.com>
2019-07-11 14:16:06 +0530

    1. Added code for Cron Scheduler

---
+++ src/main/java/com/FirstSpringBootApp.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableScheduling
+public class FirstSpringBootApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootApp.class, args);
+	}
+
+}
